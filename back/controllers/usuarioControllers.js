2c19d0729f764009aea9383d3f19684f8d6b71fd
Gabriel-Oyola <gabriel_oyola@outlook.com>
2025-02-18 09:36:05 -0300

    Se crea authenticate y se protege las peticiones

--- back/controllers/usuarioControllers.js
+++ back/controllers/usuarioControllers.js
@@ -3,27 +3,34 @@ var bcrypt = require("bcrypt-nodejs");
 var jwt = require("../helpers/jwt");
 
 const admin_registro_usuarios = async function (req, res) {
-  let data = req.body;
+  if (req.user) {
+    let data = req.body;
 
-  let usuarios = await Usuario.find({ email: data.email });
+    let usuarios = await Usuario.find({ email: data.email });
 
-  if (usuarios.length >= 1) {
+    if (usuarios.length >= 1) {
+      res.status(200).send({
+        data: undefined,
+        message: "El correo electronico ya existe",
+      });
+    } else {
+      bcrypt.hash("123456", null, null, async function (err, hash) {
+        if (err) {
+          res.status(200).send({
+            data: undefined,
+            message: "No se pudo encriptar la contraseña",
+          });
+        } else {
+          data.password = hash;
+          let usuario = await Usuario.create(data);
+          res.status(200).send({ data: usuario });
+        }
+      });
+    }
+  } else {
     res.status(200).send({
       data: undefined,
-      message: "El correo electronico ya existe",
-    });
-  } else {
-    bcrypt.hash("123456", null, null, async function (err, hash) {
-      if (err) {
-        res.status(200).send({
-          data: undefined,
-          message: "No se pudo encriptar la contraseña",
-        });
-      } else {
-        data.password = hash;
-        let usuario = await Usuario.create(data);
-        res.status(200).send({ data: usuario });
-      }
+      message: "ErrorToken",
     });
   }
 };
--- back/routes/usuario.js
+++ back/routes/usuario.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var usuarioControllers = require("../controllers/usuarioControllers");
 var mongoose = require("mongoose");
+var authenticate = require("../middlewares/authenticate");
 
 var api = express.Router();
 
 api.post(
   "/admin_registro_usuarios",
+  authenticate.decodeToken,
   usuarioControllers.admin_registro_usuarios
 );
 api.post("/login_usuario", usuarioControllers.login_usuario);
