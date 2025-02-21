402cdb2ed63d59d47f469757d00637f1355610b9
Gabriel-Oyola <gabriel_oyola@outlook.com>
2025-02-21 16:27:47 -0300

    Se crea Sidebar

--- panel/src/App.vue
+++ panel/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <Sidebar />
+    <router-view />
   </div>
 </template>
 
+<script>
+
+import { RouterView } from 'vue-router';
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  components: {
+    Sidebar,
+  }
+}
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,16 +26,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
