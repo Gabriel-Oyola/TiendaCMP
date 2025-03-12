ee0c2a9b483b9ed05d708536204eb0f8138b75db
Gabriel-Oyola <gabriel_oyola@outlook.com>
2025-03-12 07:35:39 -0300

    Listar usuario casi terminado

--- panel/src/main.js
+++ panel/src/main.js
@@ -6,7 +6,7 @@ import Notifications from "vue-notification";
 
 Vue.use(Notifications);
 Vue.config.productionTip = false;
-Vue.prototype.$url = "http://localhost:6006/api";
+Vue.prototype.$url = "http://localhost:6007/api";
 Vue.prototype.$token = localStorage.getItem("token");
 
 new Vue({
--- panel/src/views/colaboradores/indexColaboradorApp.vue
+++ panel/src/views/colaboradores/indexColaboradorApp.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <Sidebar/>
+        <Sidebar />
 
         <div class="main-content">
-            <TopNav/>
+            <TopNav />
             <div class="container-fluid">
 
                 <div class="header mt-md-5">
@@ -38,7 +38,7 @@
                                         <router-link to="/colaboradores/create" class="nav-link">
                                             Nuevo colaborador
                                         </router-link>
-                                      
+
 
                                     </li>
 
@@ -93,75 +93,55 @@
                                         <tr>
 
                                             <th>
-                                                <a class="list-sort text-muted" data-sort="item-name" href="#">Name</a>
+                                                <a class="list-sort text-muted" >Nombre</a>
                                             </th>
                                             <th>
-                                                <a class="list-sort text-muted" data-sort="item-title" href="#">Job
-                                                    title</a>
+                                                <a class="list-sort text-muted" >Rol</a>
                                             </th>
                                             <th>
-                                                <a class="list-sort text-muted" data-sort="item-email"
-                                                    href="#">Email</a>
+                                                <a class="list-sort text-muted">Email</a>
                                             </th>
+                                          
                                             <th>
-                                                <a class="list-sort text-muted" data-sort="item-phone"
-                                                    href="#">Phone</a>
+                                                <a class="list-sort text-muted" >Estado</a>
                                             </th>
-                                            <th>
-                                                <a class="list-sort text-muted" data-sort="item-score" href="#">Lead
-                                                    score</a>
-                                            </th>
-                                            <th colspan="2">
-                                                <a class="list-sort text-muted" data-sort="item-company"
-                                                    href="#">Company</a>
-                                            </th>
+                                          
                                         </tr>
                                     </thead>
                                     <tbody class="list fs-base">
-                                        <tr>
+                                        <tr v-for="item in colaboradores">
 
                                             <td>
 
                                                 <!-- Avatar -->
                                                 <div class="avatar avatar-xs align-middle me-2">
                                                     <img class="avatar-img rounded-circle"
-                                                        src="assets/img/avatars/profiles/avatar-1.jpg" alt="...">
-                                                </div> <a class="item-name text-reset" href="profile-posts.html">Dianna
-                                                    Smiley</a>
+                                                        src="/assets/img/avatar-1.jpg" alt="...">
+                                                </div> <a class="item-name text-reset">{{ item.nombre }} {{
+                                                    item.apellido }}</a>
 
                                             </td>
                                             <td>
 
                                                 <!-- Text -->
-                                                <span class="item-title">Designer</span>
+                                                <span class="item-title">{{ item.rol }}</span>
 
                                             </td>
                                             <td>
 
                                                 <!-- Email -->
-                                                <a class="item-email text-reset"
-                                                    href="mailto:john.doe@company.com">diana.smiley@company.com</a>
+                                                <a class="item-email text-reset">{{ item.email }}</a>
 
                                             </td>
+
                                             <td>
 
-                                                <!-- Phone -->
-                                                <a class="item-phone text-reset" href="tel:1-123-456-4890">(988)
-                                                    568-3568</a>
+
+                                                <span v-if="!item.estado" class="item-score badge bg-danger-soft">Desactivado</span>
+                                                <span  v-if="item.estado" class="item-score badge bg-success-soft">Activado</span>
 
                                             </td>
-                                            <td>
-
-                                                <!-- Badge -->
-                                                <span class="item-score badge bg-danger-soft">1/10</span>
-
-                                            </td>
-                                            <td>
-
-                                                <!-- Link -->
-                                                <a class="item-company text-reset" href="team-overview.html">Twitter</a>
-
-                                            </td>
+                                
                                             <td class="text-end">
 
                                                 <!-- Dropdown -->
@@ -186,74 +166,7 @@
 
                                             </td>
                                         </tr>
-                                        <tr>
 
-                                            <td>
-
-                                                <!-- Avatar -->
-                                                <div class="avatar avatar-xs align-middle me-2">
-                                                    <img class="avatar-img rounded-circle"
-                                                        src="assets/img/avatars/profiles/avatar-2.jpg" alt="...">
-                                                </div> <a class="item-name text-reset" href="profile-posts.html">Ab
-                                                    Hadley</a>
-
-                                            </td>
-                                            <td class="">
-
-                                                <!-- Text -->
-                                                <span class="item-title">Developer</span>
-
-                                            </td>
-                                            <td>
-
-                                                <!-- Email -->
-                                                <a class="item-email text-reset"
-                                                    href="mailto:john.doe@company.com">ab.hadley@company.com</a>
-
-                                            </td>
-                                            <td>
-
-                                                <!-- Phone -->
-                                                <a class="item-phone text-reset" href="tel:1-123-456-7890">(650)
-                                                    430-9876</a>
-
-                                            </td>
-                                            <td>
-
-                                                <!-- Badge -->
-                                                <span class="item-score badge bg-success-soft">8/10</span>
-
-                                            </td>
-                                            <td>
-
-                                                <!-- Link -->
-                                                <a class="item-company text-reset" href="team-overview.html">Google</a>
-
-                                            </td>
-                                            <td class="text-end">
-
-                                                <!-- Dropdown -->
-                                                <div class="dropdown">
-                                                    <a class="dropdown-ellipses dropdown-toggle" href="#" role="button"
-                                                        data-bs-toggle="dropdown" aria-haspopup="true"
-                                                        aria-expanded="false">
-                                                        <i class="fe fe-more-vertical"></i>
-                                                    </a>
-                                                    <div class="dropdown-menu dropdown-menu-end">
-                                                        <a href="#!" class="dropdown-item">
-                                                            Action
-                                                        </a>
-                                                        <a href="#!" class="dropdown-item">
-                                                            Another action
-                                                        </a>
-                                                        <a href="#!" class="dropdown-item">
-                                                            Something else here
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                            </td>
-                                        </tr>
 
                                     </tbody>
                                 </table>
@@ -342,9 +255,28 @@ import axios from 'axios'
 
 export default {
     name: 'indexColaboradorApp',
+
+    data() {
+        return {
+            colaboradores : [],       
+        }
+    },
     components: {
         Sidebar,
         TopNav
+    }, 
+    beforeMount() {
+        axios.get(this.$url + '/listar_usuarios_admin', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': this.$token
+            }
+        }).then((result => {
+            this.colaboradores = result.data;
+            console.log(colaboradores)
+        })).catch((err => {
+            console.log(err)
+        }))
     }
 }
 </script>
