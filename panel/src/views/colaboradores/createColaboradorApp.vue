8076a9bc5152812365460023b8bd79be469a3966
Gabriel-Oyola <gabriel_oyola@outlook.com>
2025-03-12 04:43:43 -0300

    Componente topnav creado

--- panel/src/views/colaboradores/createColaboradorApp.vue
+++ panel/src/views/colaboradores/createColaboradorApp.vue
@@ -3,47 +3,11 @@
     <div>
         <Sidebar />
         <div class="main-content">
+          
+            <div class="container-fluid"> 
 
-            <nav class="navbar navbar-expand-md navbar-light d-md-flex">
-                <div class="container-fluid">
-
-                    <!-- Form -->
-                    <form class="form-inline me-4 d-none d-md-flex">
-
-                    </form>
-
-                    <!-- User -->
-                    <div class="navbar-user">
-
-
-                        <!-- Dropdown -->
-                        <div class="dropdown">
-
-                            <!-- Toggle -->
-                            <a href="#" class="avatar avatar-sm avatar-online dropdown-toggle" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="/assets/img/avatar-1.jpg" alt="..."
-                                    class="avatar-img rounded-circle" style="-webkit-mask-image: none;">
-                            </a>
-
-                            <!-- Menu -->
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="./profile-posts.html" class="dropdown-item">Profile</a>
-                                <a href="./account-general.html" class="dropdown-item">Settings</a>
-                                <hr class="dropdown-divider">
-                                <a href="./sign-in.html" class="dropdown-item">Logout</a>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-            </nav>
-
-
-
-            <div class="container-fluid">
+           
+                
                 <div class="row justify-content-center">
                     <div class="col-12 col-lg-10 col-xl-8">
 
@@ -198,12 +162,14 @@
 
 <script>
 
- import Sidebar from '@/components/Sidebar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+import TopNav from '@/components/TopNav.vue'
 
 export default {
     name: 'createColaboradorApp',
     components: {
-        Sidebar
+        Sidebar, 
+        TopNav
     }
 }
 </script>
